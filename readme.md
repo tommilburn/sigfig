9d19b913c753452e76dd927d443f07571acc97bd
Tom Milburn <thomasjosephmilburn@gmail.com>
2014-01-05 15:59:33 -0500

    whole numbers functioning correctly

--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
+#Readme
+###Thomas Milburn, 2013
 This is a simple JavaScirpt significant figures calculator I'm working on to familiarize myself with Git.
--- sigfig.js
+++ sigfig.js
@@ -1,31 +1,45 @@
 function numberOfSignificantFigures(num){
 	var figures = 0;
-	if(isNumber(num)){
+		console.log("initial number: " + parseInt(num, 10));
 		sigstring = num.toString();
+		console.log("this is the number: " + parseFloat(num, 10) + " this is the number  string: " + sigstring);
 		var pos = 0
 		//whole numbers
 		if(sigstring.indexOf('.') == -1){
 			console.log("whole number");
-			return sigstring.length - pos;
-			//check for leading 0s
-			for(var pos; pos<sigstring.length; pos++){
-				if(sigstring.charAt(pos) != 0){
+			var trailzeros = 0;
+			for(var i = sigstring.length-1; i>=0; i--){
+				if(sigstring.charAt(i) != '0'){
 					break;
 				}
+				trailzeros++;
 			}
-		}	
+			return sigstring.length - trailzeros;
+		}
+
+		//decimal number
 		else{
 			console.log("decimal number");
 
 		}
-	}
-	else{
-		return -1;
-	}
-	return sigstring.length - pos;
+	return sigstring.length;
 }
 
 function isNumber(num){
 	return ! isNaN(parseFloat(num)) && isFinite(num);
 }
 
+function numberSanitizer(num){
+	numstring = num.toString();
+	//remove commas
+	numstring = num.replace(/','/g,'');
+	//remove lead zeros
+	var leadzeros = 0;
+	for(var i = 0; i < sigstring.length; i++){
+		if(sigstring.charAt(i) != '0'){
+			break;
+		}
+		leadzeros++;
+	}
+	numstring = num.substring(leadzeros, numstring.length -1);
+}
